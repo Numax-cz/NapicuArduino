d60c986c83c56a143e60991f87685988605bc181
Marcel <numax.cz@protonmail.ch>
2022-12-26 14:23:32 +0100

    Upraven vzhled kódu

--- Project 2 - ButtonDigitalRead/main.ino
+++ Project 2 - ButtonDigitalRead/main.ino
@@ -1,4 +1,3 @@
-
 const int LED_PIN = 13;
 const int BUTTON_PIN = 2;
 
@@ -6,10 +5,7 @@ int button_state = 0;
 bool diode_on = false;
 
 void setup(){
-
-
     pinMode(LED_PIN, OUTPUT);
-
     pinMode(BUTTON_PIN, INPUT);
 }
 
@@ -18,12 +14,9 @@ void loop(){
 
     if(button_state == HIGH) diode_on = !diode_on;
 
-
     if(diode_on){
         digitalWrite(LED_PIN, HIGH);
     } else digitalWrite(LED_PIN, LOW);
-
-
 
     delay(100);
 }
