d54873488ad52c66018f29f4aa8d8d6bd17b5f83
Marcel <numax.cz@protonmail.ch>
2022-12-26 14:27:35 +0100

    Opraveny chyby

--- Project 2 - ButtonDigitalRead/main.ino
+++ Project 2 - ButtonDigitalRead/main.ino
@@ -2,6 +2,8 @@ const int LED_PIN = 13;
 const int BUTTON_PIN = 2;
 
 int button_state = 0;
+int button_last_state = 0;
+
 bool diode_on = false;
 
 void setup(){
@@ -12,7 +14,11 @@ void setup(){
 void loop(){
     button_state = digitalRead(BUTTON_PIN);
 
-    if(button_state == HIGH) diode_on = !diode_on;
+    if (button_state != button_last_state) {
+        if(button_state == HIGH) diode_on = !diode_on;
+    }
+
+    button_last_state = button_state;
 
     if(diode_on){
         digitalWrite(LED_PIN, HIGH);
